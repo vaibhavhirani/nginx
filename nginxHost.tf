304f6dc5880d5b4f3e79ed38442596705b073576
vaibhavhirani <vaibhavhirani@gmail.com>
2023-05-22 19:35:58 +0530

    Adds security rules for port 443

--- nginxHost.tf
+++ nginxHost.tf
@@ -79,6 +79,19 @@ resource "azurerm_network_security_group" "nginx" {
   }
   
   security_rule {
+    name                       = "AllowHTTPS"
+    description                = "Allow HTTPS"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
     name                       = "AllowSSH"
     description                = "Allow SSH"
     priority                   = 150
